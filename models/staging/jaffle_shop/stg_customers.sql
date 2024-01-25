26bb31ed51abe2b345ccf010693e4ecf3ed17f80
Delmon <delmon.arous@nav.no>
2024-01-25 19:15:34 +0000

    Some syntax changes

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,12 +1,9 @@
 with customers as (
-
     select
         id as customer_id,
         first_name,
         last_name
-
     from `disykefravar-dev-d3d2`.jaffle_shop.customers
-
 )
 
 select * from customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,13 +1,10 @@
 with orders as (
-
     select
         id as order_id,
         user_id as customer_id,
         order_date,
         status
-
     from `disykefravar-dev-d3d2`.jaffle_shop.orders
-
 )
 
 select * from orders
