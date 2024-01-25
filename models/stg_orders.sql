c643ac6525e22fd9f6eba923f161e4fda4fff992
Delmon <delmon.arous@nav.no>
2024-01-25 15:10:40 +0000

    Created stg_orders.sql

---
+++ models/stg_orders.sql
@@ -0,0 +1,13 @@
+with orders as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from `disykefravar-dev-d3d2`.jaffle_shop.orders
+
+)
+
+select * from orders
